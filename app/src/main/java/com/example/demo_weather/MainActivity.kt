ddbefe2d390b610dea73138b517b4199d0839b9a
yashmore142 <yashmore142f@gmail.com>
2024-10-21 17:53:49 +0530

    demo

--- app/src/main/java/com/example/demo_weather/MainActivity.kt
+++ app/src/main/java/com/example/demo_weather/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                     
                     Greeting("Android")
                 }
             }
